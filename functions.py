e3ce531ebfb2387d66fd75cc49cfabde6b5480ef
luisca12 <solera.luis12@gmail.com>
2024-09-27 11:10:25 -0600

    complianceFiles Added

--- functions.py
+++ functions.py
@@ -80,7 +80,7 @@ def requestLogin(validIPs):
         try:
             username = input("Please enter your username: ")
             password = getpass.getpass("Please enter your password: ")
-            # execPrivPassword = getpass.getpass("Please input your enable password: ")
+            #execPrivPassword = getpass.getpass("Please input your enable password: ")
 
             for deviceIP in validIPs:
                 netDevice = {
